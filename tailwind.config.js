e90254e67183b25c354a807f381a03d7bc45af19
daihoc24 <21130505@st.hcmuaf.edu.vn>
2025-03-30 14:18:34 +0700

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'color1': '#87c84a',
+        'color2': 'rgb(113 113 113)',
+      },
+    },
+  },
+  plugins: [],
+}/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
