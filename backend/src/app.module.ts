a78156aa1621ee0df2885020716a6f788867a895
Long <145737815+TyntyNguyen0710@users.noreply.github.com>
2025-04-09 19:51:51 +0700

    product, strategy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BankAccountsModule } from './bank_accounts/bank_accounts.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
+import { StrategyModule } from './strategy/strategy.module';
 
 @Module({
-  imports: [AuthModule, BankAccountsModule],
+  imports: [AuthModule, BankAccountsModule, OrderModule, ProductModule, StrategyModule],
   controllers: [AppController],
   providers: [AppService],
 })
