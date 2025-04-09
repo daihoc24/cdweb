b4b669b24a2b6b453a8575ac22bded50cc0b8800
Long <145737815+TyntyNguyen0710@users.noreply.github.com>
2025-04-09 19:33:53 +0700

    db user, account bank, auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BankAccountsModule } from './bank_accounts/bank_accounts.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, BankAccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
