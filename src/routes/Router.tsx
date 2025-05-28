1ef65201e0bc719abe62d25c4435e5f33facc6be
Long <145737815+TyntyNguyen0710@users.noreply.github.com>
2025-05-29 00:33:25 +0700

    page/Home
    - create home page, tsx, scss
    - add service prod, api fetch prod
    - data prod

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomeLayout from "../layout/HomeLayout/HomeLayout";
 import Login from "../pages/login/Login";
+import Home from "../pages/Home/Home";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomeLayout />}>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Route >
     </Routes >
     )
--- src/services/product.tsx
+++ src/services/product.tsx
@@ -1,2 +1,19 @@
-class ProductService {}
+import { AxiosResponse } from "axios";
+import {
+    addComment,
+    addProduct,
+    listProduct,
+    ProductResponse,
+} from "../interfaces/product";
+
+import { request } from "../config/api";
+class ProductService {
+    fetchProductApi(): Promise<AxiosResponse<ProductResponse>> {
+        return request({
+            url: `/Product/GetListProduct`,
+            method: "GET",
+        });
+    }
+}
+
 export const productService: ProductService = new ProductService();
